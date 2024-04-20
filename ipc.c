4dd8bf0a0d134476628b02c27abca932757334bf
diego rousseaux <diego.rousseaux@student.vinci.be>
2024-04-20 13:56:19 +0200

    feat:ipc

--- ipc.c
+++ ipc.c
@@ -1,1 +1,35 @@
-// Mémoire Partagée
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
+
+#include "utils_v1.h"
+#include "ipc.h"
+
+void createIpc() {
+    // Mémoire Partagée
+    // IPC creation and initialization
+    sshmget(RAKING_SHM_KEY, 2 * sizeof(int), IPC_CREAT | IPC_EXCL | PERM);
+    sem_create(RAKING_SEM_KEY, 1, IPC_CREAT | IPC_EXCL | PERM, 1);   
+    printf("IPCs created.\n");
+}
+
+void detachIpc() {
+    // IPC destruction
+    printf("Destroying IPCs...\n");
+    int shm_id = sshmget(RAKING_SHM_KEY, 2 * sizeof(int), 0);
+    sshmdelete(shm_id);
+  
+    int sem_id = sem_get(RAKING_SEM_KEY, 2);
+    sem_delete(sem_id);
+
+    printf("IPCs freed.\n");
+}
+
+int* getSharedMemory() {
+    int shid = sshmget(RAKING_SHM_KEY, 2 * sizeof(int), 0);
+  
+    int* memory = sshmat(shid);
+   
+    return memory;
+}
